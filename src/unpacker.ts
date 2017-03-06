f44fc028982d4bd9bafb86ce7bd2942843d0de61
Nikita Basargin <nbasargin@web.de>
2017-03-06 13:34:56 +0100

    unpacker: safer path concatenation; added check for same input & output name

--- src/unpacker.ts
+++ src/unpacker.ts
@@ -3,13 +3,15 @@ export class Unpacker {
     fs = require('fs');
     unzip = require('unzip');
     zlib = require('zlib');
+    path = require('path');
+
 
     /**
      * Unpacks a .gz file asynchronously into the specified folder.
      * @param gzipFilePath          path to the .gz file
      * @param outputFolderPath      path
      * @param filename              (optional) name for the output file
-     * @param callback              (optional) function that will be called once the file is unpacked
+     * @param callback              (optional) function that will be called once the file is unpacked (no parameters)
      */
     public unpackGZipFileToFile(gzipFilePath : string,
                                 outputFolderPath : string,
@@ -26,11 +28,19 @@ export class Unpacker {
             let spl = gzipFilePath.split("/");
             outputFileName = spl[spl.length - 1].replace(".gz", "");
         }
+        let outputFilePath : string = this.path.join(outputFolderPath, outputFileName);
+        outputFilePath = this.path.normalize(outputFilePath);
+        gzipFilePath = this.path.normalize(gzipFilePath);
+        //do not use the same name for input & output
+        if (outputFilePath == gzipFilePath) {
+            outputFilePath += ".unpacked";
+            console.warn("Using same name for input & output files! Output file was changed to " + outputFilePath);
+        }
 
         // unpack
         const gunzip = this.zlib.createGunzip();
         let input = this.fs.createReadStream(gzipFilePath);
-        let output = this.fs.createWriteStream(outputFolderPath + "/" + outputFileName);
+        let output = this.fs.createWriteStream(outputFilePath);
         input.pipe(gunzip).pipe(output);
 
         // call callback if defined
@@ -40,6 +50,8 @@ export class Unpacker {
     }
 
     /**
+     * @deprecated not tested well yet
+     *
      * Unpacks a .zip file into the specified folder (must already exist). The .zip file must be plain since subfolders
      * are not yet supported.
      *
