e93c073480fea715200ca9950b77c1677f54f818
Nikita Basargin <nbasargin@web.de>
2017-03-28 19:30:31 +0200

    refactoring: moved all sample data generators from TestRuns to a separate file

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,8 @@ import {TestRuns} from "./test-runs";
 // TestRuns.testExtractAllEnglishPages();   // WET file must already be downloaded!
 // TestRuns.createFilteredSampleDataForGroups3_4();
 // TestRuns.getWebsiteByURL();
-TestRuns.extractPagesByURL();
+import {SampleDataGenerator} from "./sample-data/sample-data-generator";
+SampleDataGenerator.extractPagesByURL();
 
 
 // These runs require the WET file to be already downloaded and unpacked:
