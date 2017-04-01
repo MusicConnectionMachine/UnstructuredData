eca2ecd58195ddd5d1714b31d34b738ccd034c13
Nikita Basargin <nbasargin@web.de>
2017-04-01 17:15:22 +0200

    map to set helper

--- src/webpage-digester.ts
+++ src/webpage-digester.ts
@@ -23,6 +23,21 @@ export class WebPageDigester {
     }
 
     /**
+     * Takes all the keys from the map and returns a set of them.
+     * @param map
+     * @returns {Set<string>}
+     */
+    private static mapToSet(map : Map<string, string>) : Set<string> {
+        let set : Set<string> = new Set();
+
+        for (let [term] of map.entries()) {
+            // strangely "for (let term in map)"  fails the tests
+            set.add(term);
+        }
+        return set;
+    }
+
+    /**
      * Set the main filter of the digester which will determine the indexes of all matches
      * Be careful which filter to use here as not all filters will give you exact matches!
      * @param filterConstructor                             Class of the filter
@@ -41,8 +56,7 @@ export class WebPageDigester {
      */
     public setPreFilter<T extends Filter> (filterConstructor : new (terms? : Set<string>) => T) : WebPageDigester {
         // construct a set from the map
-        let set : Set<string> = new Set();
-        for (let [term /*, id*/] of this.termToIDMap.entries())  set.add(term);
+        let set : Set<string> = WebPageDigester.mapToSet(this.termToIDMap);
 
         this.preFilterInstance = new filterConstructor(set);
         return this;
@@ -89,9 +103,7 @@ export class WebPageDigester {
         // create new mainFilterInstance from this.searchTerms if not present
         if (!this.mainFilterInstance) {
             // construct a set from the map
-            let set : Set<string> = new Set();
-            for (let [term /*, id*/] of this.termToIDMap.entries())  set.add(term);
-
+            let set : Set<string> = WebPageDigester.mapToSet(this.termToIDMap);
             this.mainFilterInstance = new this.mainFilter(set);
         }
 
