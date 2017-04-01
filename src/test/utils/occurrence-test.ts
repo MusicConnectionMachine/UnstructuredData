aed90f468f18ed7fef3eff4d428e2aea89b13044
Nikita Basargin <nbasargin@web.de>
2017-04-01 15:10:37 +0200

    occurrence test map <-> array test

--- src/test/utils/occurrence-test.ts
+++ src/test/utils/occurrence-test.ts
@@ -1,31 +1,29 @@
 import "mocha";
-import {IndexFilterResult} from "../../utils/index-filter-result";
+import {Occurrence} from "../../utils/occurrence";
+import {Term} from "../../utils/term";
 let assert = require("chai").assert;
 
-describe("IndexFilterResult ifrMapToArray & ifrArrayToMap", () => {
-    it("should convert a occurrence map into an array of occurrences .ifrMapToArray()", () => {
-        let occurrenceMap = new Map();
-        occurrenceMap.set("test", [1, 42]);
-        occurrenceMap.set("random", [69]);
-        let occurrenceArray = [];
-        occurrenceArray.push({term: "test", positions: [1, 42]});
-        occurrenceArray.push({term: "random", positions: [69]});
-        assert.deepEqual(occurrenceArray, IndexFilterResult.ifrMapToArray(occurrenceMap));
+describe("Occurrence", () => {
+    it("should work: arrayToMap and mapToArray", () => {
+        let occ1 = new Occurrence(new Term("term1", "1"), [42]);
+        let occ2 = new Occurrence(new Term("term2", "2"), [43]);
+        let occ3 = new Occurrence(new Term("term3", "3"), [44]);
+        let occArrExpected = [occ1, occ2, occ3];
+
+        let occMapResult = Occurrence.occArrayToMap(occArrExpected);
+        let occArrResult = Occurrence.occMapToArr(occMapResult);
+
+        // deepEqual is not working on Maps: returns OK even if two maps are obviously different
+        assert.isTrue(JSON.stringify(occMapResult.get("term1")) == JSON.stringify(["1", [42]]));
+        assert.isTrue(JSON.stringify(occMapResult.get("term2")) == JSON.stringify(["2", [43]]));
+        assert.isTrue(JSON.stringify(occMapResult.get("term3")) == JSON.stringify(["3", [44]]));
+        assert.isTrue(JSON.stringify(occArrExpected) == JSON.stringify(occArrResult));
+
+        // test false
+        assert.isFalse(JSON.stringify(occMapResult.get("term2")) == JSON.stringify(["3", [44]]));
+        assert.isFalse(JSON.stringify(occMapResult.get("term3")) == JSON.stringify(["2", [43]]));
+        assert.isFalse(JSON.stringify(occMapResult.get("term1")) == JSON.stringify(["3", [44]]));
+
     });
-    it("should convert a an array of occurrences into a map .ifrArrayToMap()", () => {
-        let occurrenceMap = new Map();
-        occurrenceMap.set("test", [1, 42]);
-        occurrenceMap.set("random", [69]);
-        let occurrenceArray = [];
-        occurrenceArray.push({term : "test", positions: [1, 42]});
-        occurrenceArray.push({term : "random", positions: [69]});
-        assert.deepEqual(occurrenceMap, IndexFilterResult.ifrArrayToMap(occurrenceArray));
-    });
-    it(".ifrMapToArray() should be inverse to .ifrArrayToMap()", () => {
-        let occurrenceArray = [];
-        occurrenceArray.push({term : "test", positions: [1, 42]});
-        occurrenceArray.push({term : "random", positions: [69]});
-        let occurrenceMap = IndexFilterResult.ifrArrayToMap(occurrenceArray);
-        assert.deepEqual(occurrenceArray, IndexFilterResult.ifrMapToArray(occurrenceMap));
-    })
+
 });
