b1a25b9ccfb80cbe8bfe0900fd0904e9e32fe2f0
Nikita Basargin <nikita@userlane.com>
2017-04-04 00:22:07 +0200

    getting artists from the DB

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
-// TEST RUNS
-// Don't run multiple runs simultaneously!
-import {TestRuns} from "./test-runs";
+import {TermLoader} from "./utils/term-loader";
+
+TermLoader.loadFromDB((err, entities) => {
+    if (err) {
+        console.log("SHIT!\n", err);
+        return;
+    }
+    console.log("\n\n\n\n");
+    console.log("entities:\n", entities);
+
+});
