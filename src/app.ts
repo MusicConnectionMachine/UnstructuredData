72955d851cf884d4d6115466d2de59e25f967a04
Nikita Basargin <nbasargin@web.de>
2017-04-09 12:33:20 +0200

    moved cli call to app.ts, fixed wet naming, more error checks

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
+#!/usr/bin/env node
+
+// first line is mandatory in JS, not sure about TS
+
+// try to run this with
+// node out/app.js -w 0:99 -d localhost:1234 -a db:pwd -b acc:container -k blob-key -t 1 -c crawl
+// node out/app.js -h
+
 import {ProcessingManager} from "./processing-manager";
 import {Worker} from "./worker";
+import {CLI} from "./cli";
+
+
+
+CLI.initCLI();
+
+console.log("Start config:\n", CLI.parameters);
+
 
 // Don't touch this otherwise Felix will kill you :P
 ProcessingManager.run();
--- src/cli.ts
+++ src/cli.ts
@@ -1,24 +1,6 @@
-#!/usr/bin/env node
-
-// try to run this with
-// node out/cli.js -w 0:99 -d localhost:1234 -a db:pwd -b acc:container -k blob-key -t 1 -c crawl
-// node out/cli.js -h
-
-const commander = require('commander');
-commander
-    //.arguments('<file1>')
-    //.arguments('<file2>')
-    .option('-w, --wetRange [start]:[end]', 'the subset of WET files to process, e.g. "0:99"')
-    .option('-d, --db-location [host]:[port]', 'database location, e.g. "127.0.0.1:5432"')
-    .option('-a, --db-access [user]:[password]', 'database access, e.g. "USER:PASSWORD"')
-    .option('-b, --blob-location [account]:[container]', 'blob storage location, e.g. "wetstorage:websites"')
-    .option('-k, --blob-key [storageKey]', 'blob storage access key, e.g. "AZURE_KEY_HERE"')
-    .option('-t, --threads [number]', 'number of worker threads, e.g. "4"')
-    .option('-c, --crawl [version]', 'common crawl version, e.g. "CC-MAIN-2017-13"')
-    .parse(process.argv);
-
 export class CLI {
 
+    private static commander = require('commander');
 
     public static parameters = {
         wetFrom: 0,
@@ -34,6 +16,29 @@ export class CLI {
         crawlVersion: "CC-MAIN-2017-13"
     };
 
+    public static initCLI() {
+
+        // init commander
+        CLI.commander
+            .option('-w, --wet-range [start]:[end]', 'the subset of WET files to process, e.g. "0:99"')
+            .option('-d, --db-location [host]:[port]', 'database location, e.g. "127.0.0.1:5432"')
+            .option('-a, --db-access [user]:[password]', 'database access, e.g. "USER:PASSWORD"')
+            .option('-b, --blob-location [account]:[container]', 'blob storage location, e.g. "wetstorage:websites"')
+            .option('-k, --blob-key [storageKey]', 'blob storage access key, e.g. "AZURE_KEY_HERE"')
+            .option('-t, --threads [number]', 'number of worker threads, e.g. "4"')
+            .option('-c, --crawl [version]', 'common crawl version, e.g. "CC-MAIN-2017-13"')
+            .parse(process.argv);
+
+
+        // environment variables override default hardcoded values
+        CLI.parseEnvVars();
+        // config overrides environment vars
+        CLI.parseConfigFile();
+        // command line overrides all
+        CLI.parseCmdOptions();
+
+    }
+
     public static parseEnvVars() {
         // TODO: replace default values in CLI.parameters with environment variables
     }
@@ -43,57 +48,63 @@ export class CLI {
     }
 
     public static parseCmdOptions() {
-        if (commander.wetRange) {
-            // [start]:[end]
-            let splitted = commander.wetRange.split(":", 2);
-            let wetFrom = parseInt(splitted[0]);
-            let wetTo = parseInt(splitted[1]);
-            if (wetFrom) CLI.parameters.wetFrom = wetFrom;
-            if (wetTo)   CLI.parameters.wetTo = wetTo;
+
+        if (CLI.commander.wetRange) {
+            if (!CLI.commander.wetRange.split) {
+                console.warn("invalid --wet-range [start]:[end]");
+            } else {
+                let splitted = CLI.commander.wetRange.split(":", 2);
+                let wetFrom = parseInt(splitted[0]);
+                let wetTo = parseInt(splitted[1]);
+                if (wetFrom) CLI.parameters.wetFrom = wetFrom;
+                if (wetTo)   CLI.parameters.wetTo = wetTo;
+            }
         }
-        if (commander.dbLocation) {
-            // [host]:[port]
-            let splitted = commander.dbLocation.split(":", 2);
-            CLI.parameters.dbHost = splitted[0];
-            let dbPort = parseInt(splitted[1]);
-            if (dbPort) CLI.parameters.dbPort = dbPort;
+
+        if (CLI.commander.dbLocation) {
+            if (!CLI.commander.dbLocation.split) {
+                console.warn("invalid --db-location [host]:[port]");
+            } else {
+                let splitted = CLI.commander.dbLocation.split(":", 2);
+                CLI.parameters.dbHost = splitted[0];
+                let dbPort = parseInt(splitted[1]);
+                if (dbPort) CLI.parameters.dbPort = dbPort;
+            }
         }
-        if (commander.dbAccess) {
-            //[user]:[password]
-            let splitted = commander.dbAccess.split(":", 2);
-            CLI.parameters.dbUser = splitted[0];
-            CLI.parameters.dbPW = splitted[1];
+
+        if (CLI.commander.dbAccess) {
+            if (!CLI.commander.dbAccess.split) {
+                console.warn("invalid --db-access [user]:[password]");
+            } else {
+                let splitted = CLI.commander.dbAccess.split(":", 2);
+                CLI.parameters.dbUser = splitted[0];
+                CLI.parameters.dbPW = splitted[1];
+            }
         }
-        if (commander.blobLocation) {
-            //[account]:[container]
-            let splitted = commander.blobLocation.split(":", 2);
-            CLI.parameters.blobAccount = splitted[0];
-            CLI.parameters.blobContainer = splitted[1];
+
+        if (CLI.commander.blobLocation) {
+            if (!CLI.commander.blobLocation.split) {
+                console.warn("invalid --blob-location [account]:[container]");
+            } else {
+                let splitted = CLI.commander.blobLocation.split(":", 2);
+                CLI.parameters.blobAccount = splitted[0];
+                CLI.parameters.blobContainer = splitted[1];
+            }
         }
-        if (commander.blobKey) {
-            CLI.parameters.blobKey = commander.blobKey;
+
+        if (CLI.commander.blobKey) {
+            CLI.parameters.blobKey = CLI.commander.blobKey;
         }
-        if (commander.threads) {
-            let threads = parseInt(commander.threads);
+
+        if (CLI.commander.threads) {
+            let threads = parseInt(CLI.commander.threads);
             if (threads) CLI.parameters.threads = threads;
         }
-        if (commander.crawl) {
-            CLI.parameters.crawlVersion = commander.crawl;
+
+        if (CLI.commander.crawl) {
+            CLI.parameters.crawlVersion = CLI.commander.crawl;
         }
 
     }
 
 }
-
-// environment variables override default hardcoded values
-CLI.parseEnvVars();
-// config overrides environment vars
-CLI.parseConfigFile();
-// command line overrides all
-CLI.parseCmdOptions();
-
-
-console.log("Starting with parms:\n", CLI.parameters);
-
-// TODO: start ProcessingManager & Workers
-
