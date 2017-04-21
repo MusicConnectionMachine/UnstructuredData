a50a7e9f951ba90006dc2920cea55235c09aca4c
lgfrbcsgo <lgfrbcsgo@gmail.com>
2017-04-21 16:30:20 +0200

    Codacy, please don't be a bully :(

--- scripts/server_master.sh
+++ scripts/server_master.sh
@@ -1,17 +1,19 @@
-sudo apt-get install sshpass 
+#!/bin/bash
+
+sudo apt-get install sshpass
 
 sshPW = "I may be stupid but not that stupid"
 
 size = 665
 
 start = 16
-end = $(expr $start + $size)
+end = $((start + size))
 
 for ip in 10.0.0.{2..254}; do
   if ping -i 0.2 -c 1 $ip | grep "1 received"
   then
     sshpass -p "$sshPW" ssh "group2@$ip" < "nohup sh -c 'cd UnstructuredData/out && node app.js -t 4 -w $start:$end -p 8 -c CC-MAIN-2017-13' &"
-    start = $(expr $start + $size)
-    end = $(expr $end + $size)
+    start = $((start + size))
+    end = $((end + $size))
   fi
 done
