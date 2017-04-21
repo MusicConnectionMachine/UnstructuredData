d1b692a5df54c995eea27c723bcb26ea72a0cc85
lgfrbcsgo <lgfrbcsgo@gmail.com>
2017-04-21 16:34:47 +0200

    Just add your green check mark already :(

--- scripts/server_master.sh
+++ scripts/server_master.sh
@@ -14,6 +14,6 @@ for ip in 10.0.0.{2..254}; do
   then
     sshpass -p "$sshPW" ssh "group2@$ip" < "nohup sh -c 'cd UnstructuredData/out && node app.js -t 4 -w $start:$end -p 8 -c CC-MAIN-2017-13' &"
     start = $((start + size))
-    end = $((end + $size))
+    end = $((end + size))
   fi
 done
