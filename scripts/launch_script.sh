a5a22ac34c7ef80aa377cd5f30e8548c98591383
lgfrbcsgo <lgfrbcsgo@gmail.com>
2017-04-21 16:14:47 +0200

    Added scripts for deploying VMs on Azure

---
+++ scripts/launch_script.sh
@@ -0,0 +1,4 @@
+sshPW = "I may be stupid but not that stupid"
+masterVmIP = "127.0.0.1"
+
+sshpass -p '$sshPW' ssh 'group2@$masterVmIP' < server_master.sh
---
+++ scripts/server_master.sh
@@ -0,0 +1,17 @@
+sudo apt-get install sshpass 
+
+sshPW = "I may be stupid but not that stupid"
+
+size = 665
+
+start = 16
+end = $(expr $start + $size)
+
+for ip in 10.0.0.{2..254}; do
+  if ping -i 0.2 -c 1 $ip | grep "1 received"
+  then
+    sshpass -p "$sshPW" ssh "group2@$ip" < "nohup sh -c 'cd UnstructuredData/out && node app.js -t 4 -w $start:$end -p 8 -c CC-MAIN-2017-13' &"
+    start = $(expr $start + $size)
+    end = $(expr $end + $size)
+  fi
+done
