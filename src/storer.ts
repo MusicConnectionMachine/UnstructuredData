a0045d7860656e902c993aabb2879fa5564195be
felixschorer <felixschorer@users.noreply.github.com>
2017-04-29 18:49:17 +0200

    Undid most changes, according to sequelize docs this should work...

--- src/storer.ts
+++ src/storer.ts
@@ -199,7 +199,17 @@ export class Storer {
             });
         });
 
-        let onError = (err : Error) => {
+        this.context.sequelize.transaction(transaction => {
+            return this.context.models.websites.bulkCreate(websiteInserts, {transaction: transaction}).then(() => {
+                return this.context.models.contains.bulkCreate(containsInserts, {transaction: transaction});
+            });
+        }).then(() => {
+            winston.info("Successfully offloaded data to DB!");
+            this.websites = [];
+            if(callback) {
+                callback();
+            }
+        }).catch(err => {
             if (retries > 0) {
                 winston.error("Failed to offload data to DB! Retrying in 60 seconds!", err);
                 setTimeout(() => this.connectToDB(() => this.flushDatabase(callback, retries - 1)), 60000);
@@ -207,20 +217,6 @@ export class Storer {
                 winston.error("Finally failed to offload data to DB! Calling callback!", err);
                 if (callback) callback(err);
             }
-        };
-
-        this.context.sequelize.transaction(transaction => {
-            return this.context.models.websites.bulkCreate(websiteInserts,
-                { transaction: transaction} ).then(() => {
-
-                return this.context.models.contains.bulkCreate(containsInserts, { transaction: transaction });
-            }).catch(onError);
-        }).then(() => {
-            winston.info("Successfully offloaded data to DB!");
-            this.websites = [];
-            if(callback) {
-                callback();
-            }
-        }).catch(onError);
+        });
     }
 }
