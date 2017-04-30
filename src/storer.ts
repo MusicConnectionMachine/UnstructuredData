37b1e4ec1e4189b9108e857b6a3862eee97e2437
felixschorer <felixschorer@users.noreply.github.com>
2017-04-30 21:36:12 +0200

    Added option to save web page metadata as JSON

--- src/storer.ts
+++ src/storer.ts
@@ -7,69 +7,77 @@ import * as uuid from "uuid/v4";
 
 export class Storer {
 
-    private container;
-    private blobService;
-    private dbConnectionString : string;
-    // this guy has to be initialized with connectToDB()!
-    private context;
-    //This could be left out but it makes it easier for the other groups to access the blobs if we store them with fqdn
+    private storageService;
+    private blobContainer : string;
     private blobPrefix : string;
+    private jsonContainer : string;
 
-    private blob : {name : string, entries : Array<WebPage>};
-    private websites : Array<{id : string, url : string, blobUrl : string, occurrences : Array<Occurrence>}> = [];
+    private saveMetadataJSON : boolean;
 
-    constructor(blobParams : {[param : string] : string }, dbParams : {[param : string] : string}){
+    private context;
+    private connectToDB : (callback : (err? : Error) => void, retries? : number) => void = (callback) => {
+        callback(new TypeError("method not defined"));
+    };
 
-        let blobAccount = blobParams["blobAccount"];
-        let blobContainer = blobParams["blobContainer"];
-        let blobKey = blobParams["blobKey"];
+    private blob : {name : string, content : string} = {name: uuid(), content: ""};
+    private websites : Array<{url : string, blobUrl : string, occurrences : Array<Occurrence>}> = [];
 
-        this.blobService = azure.createBlobService(
+    /**
+     * This object is responsible for saving web page content and metadata. Content will be stored in the
+     * supplied Azure blob storage container. Metadata will be saved in a separate storage container or in
+     * a Postgres DB if supplied
+     * @param blobParams                                    details of Azure storage account and containers
+     * @param dbParams                                      (optional) credentials for Postgres DB
+     */
+    constructor(blobParams : {blobAccount : string, blobKey : string, blobContainer : string, jsonContainer : string },
+                dbParams? : {dbUser : string, dbPW : string, dbHost : string, dbPort : string, dbName : string}){
+
+        let blobAccount = blobParams.blobAccount;
+        let blobKey = blobParams.blobKey;
+        this.blobContainer = blobParams.blobContainer;
+        this.jsonContainer = blobParams.jsonContainer;
+
+        this.storageService = azure.createBlobService(
             blobAccount,
             blobKey
         );
-        this.blobPrefix = 'https://' + blobAccount +
-            '.blob.core.windows.net/' + blobContainer + '/';
-        this.container = blobContainer;
-        this.blobService.createContainerIfNotExists(blobContainer, err => {
-            if (err) {
-                winston.error("Couldn't create blob container!", err);
-            }
-        });
+        this.blobPrefix = 'https://' + blobAccount + '.blob.core.windows.net/' + this.blobContainer + '/';
 
-        this.dbConnectionString = "postgresql://"
-            + dbParams["dbUser"] + ":"
-            + dbParams["dbPW"] + "@"
-            + dbParams["dbHost"] + ":"
-            + dbParams["dbPort"] + "/"
-            + dbParams["dbName"];
-    }
+        if (dbParams) {
+            this.connectToDB = (callback : (err? : Error) => void, retries? : number) => {
+                let dbConnectionString = "postgresql://"
+                    + dbParams.dbUser + ":"
+                    + dbParams.dbPW + "@"
+                    + dbParams.dbHost + ":"
+                    + dbParams.dbPort + "/"
+                    + dbParams.dbName;
 
-
-    private connectToDB(callback : (err? : Error) => void, retries? : number) : void {
-
-        //Connect to database using api's index
-        require('../api/database').connect(this.dbConnectionString, context => {
-            //Store context
-            this.context = context;
-            /*
-             Make sure that syncing to database is synchronous.
-             Not that there is no {force: true} option here: We don't want to overwrite
-             existing tables.
-             */
-            context.sequelize.sync().then(() => {
-                winston.info("Connection to DB established!");
-                callback();
-            }, (err) => {
-                if (retries > 0) {
-                    winston.error("Failed to connect to DB. Retrying in 60 seconds!", err);
-                    setTimeout(() => this.connectToDB(callback, retries - 1), 6000);
-                } else {
-                    winston.error("Finally failed to connect to DB. Calling Callback!", err);
-                    callback(err);
-                }
-            });
-        });
+                //Connect to database using api's index
+                require('../api/database').connect(dbConnectionString, context => {
+                    //Store context
+                    this.context = context;
+                    /*
+                     Make sure that syncing to database is synchronous.
+                     Not that there is no {force: true} option here: We don't want to overwrite
+                     existing tables.
+                     */
+                    context.sequelize.sync().then(() => {
+                        winston.info("Connection to DB established!");
+                        callback();
+                    }, (err) => {
+                        if (retries > 0) {
+                            winston.error("Failed to connect to DB. Retrying in 60 seconds!", err);
+                            setTimeout(() => this.connectToDB(callback, retries - 1), 6000);
+                        } else {
+                            winston.error("Finally failed to connect to DB. Calling Callback!", err);
+                            callback(err);
+                        }
+                    });
+                });
+            };
+        } else {
+            this.saveMetadataJSON = true;
+        }
     }
 
 
@@ -79,8 +87,8 @@ export class Storer {
      * It will collect data until you call .flush()
      * @param webPage
      */
-    public storeWebsite(webPage : WebPage) : void {
-        this.storeWebsiteBlob(webPage).storeWebsiteMetadata(webPage);
+    public storeWebsite(webPage : WebPage) : Storer {
+        return this.storeWebsiteBlob(webPage).storeWebsiteMetadata(webPage);
     }
 
 
@@ -88,14 +96,12 @@ export class Storer {
      * This doesn't actually save to the DB immediately. It will collect data until you call .flushDatabase()
      * @param webPage
      */
-    private storeWebsiteMetadata(webPage : WebPage) {
+    private storeWebsiteMetadata(webPage : WebPage) : Storer {
         this.websites.push({
-            id: uuid(),
             url: webPage.getURI(),
             blobUrl: this.blob ? this.blobPrefix + this.blob.name : null,
             occurrences: webPage.occurrences
         });
-
         return this;
     }
 
@@ -108,14 +114,8 @@ export class Storer {
      *
      * @param webPage       WebPage object to be stored
      */
-    private storeWebsiteBlob(webPage : WebPage) {
-        if (!this.blob) {
-            this.blob = {
-                name: uuid(),
-                entries: []
-            };
-        }
-        this.blob.entries.push(webPage);
+    private storeWebsiteBlob(webPage : WebPage) : Storer {
+        this.blob.content += webPage.toWARCString();
         return this;
     }
 
@@ -125,33 +125,37 @@ export class Storer {
             if(err) {
                 return callback(err);
             }
-            this.flushDatabase(err => {
-                if(err) {
-                    return callback(err);
-                }
-                callback();
-            }, retries)
+            if (this.saveMetadataJSON) {
+                this.flushMetadataJSON(err => {
+                    if(err) {
+                        return callback(err);
+                    }
+                    callback();
+                }, retries)
+            } else {
+                this.flushMetadataDB(err => {
+                    if(err) {
+                        return callback(err);
+                    }
+                    callback();
+                }, retries)
+            }
         }, retries);
     }
 
 
     private flushBlob(callback? : (err? : Error) => void, retries? : number) : void {
 
-        let blobContent = "";
-        for (let entry of this.blob.entries) {
-            blobContent += entry.toWARCString();
-        }
-
         let blobName = this.blob.name;
-        Unpacker.compressStringToBuffer(blobContent, (err, compressedBlobContent) => {
+        Unpacker.compressStringToBuffer(this.blob.content, (err, compressedBlobContent) => {
             if (err) {
                 if (callback) callback(err);
                 return;
             }
-            this.blobService.createBlockBlobFromText(this.container, blobName, compressedBlobContent, (err) => {
+            this.storageService.createBlockBlobFromText(this.blobContainer, blobName, compressedBlobContent, (err) => {
                 if (!err) {
                     winston.info("Successfully offloaded blob!");
-                    this.blob = undefined;
+                    this.blob = {name: uuid(), content: ""};
                     if (callback) callback();
                 } else if (retries > 0) {
                     winston.error("Failed to offload blob! Retrying in 60 seconds!", err);
@@ -165,13 +169,30 @@ export class Storer {
     }
 
 
-    private flushDatabase(callback ? : (err? : Error) => void, retries? : number) : void {
+    private flushMetadataJSON(callback ? : (err? : Error) => void, retries? : number) : void {
+        let blobName = uuid() + ".json";
+        let blobContent = JSON.stringify(this.websites);
+        this.storageService.createBlockBlobFromText(this.jsonContainer, blobName, blobContent, (err) => {
+            if (!err) {
+                winston.info("Successfully offloaded metadata blob!");
+                if (callback) callback();
+            } else if (retries > 0) {
+                winston.error("Failed to offload metadata blob! Retrying in 60 seconds!", err);
+                setTimeout(() => this.flushMetadataJSON(callback, retries - 1), 60000);
+            } else {
+                winston.error("Finally failed to offload metadata blob! Calling callback!", err);
+                if (callback) callback(err);
+            }
+        });
+    }
+
+    private flushMetadataDB(callback ? : (err? : Error) => void, retries? : number) : void {
 
         // lazily load sequelize context
         if (!this.context) {
             return this.connectToDB((err) => {
                 if (!err) {
-                    this.flushDatabase(callback, retries);
+                    this.flushMetadataDB(callback, retries);
                 } else if (callback) {
                     callback(err);
                 }
@@ -212,7 +233,7 @@ export class Storer {
         }).catch(err => {
             if (retries > 0) {
                 winston.error("Failed to offload data to DB! Retrying in 60 seconds!", err);
-                setTimeout(() => this.connectToDB(() => this.flushDatabase(callback, retries - 1)), 60000);
+                setTimeout(() => this.connectToDB(() => this.flushMetadataDB(callback, retries - 1)), 60000);
             } else {
                 winston.error("Finally failed to offload data to DB! Calling callback!", err);
                 if (callback) callback(err);
